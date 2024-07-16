d47f5233420b6395bed4d0034b2f4f4c6c27f086
HotCheetos101 <120177674+HotCheetos101@users.noreply.github.com>
2024-07-17 02:36:39 +0800

    displays only icon

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,22 +30,24 @@ return [
 
     'disks' => [
         'local' => [
-        'driver' => 'local',
-        'root' => storage_path('app'),
+            'driver' => 'local',
+            'root' => storage_path('app'),
         ],
         'public' => [
-        'driver' => 'local',
-        'root' => storage_path('app/public'),
-        'url' => env('APP_URL').'/storage',
-        'visibility' => 'public',
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
         ],
         'employee_photos' => [
-        'driver' => 'local',
-        'root' => storage_path('app/employee_photos'),
-        'url' => env('APP_URL').'/storage/employee_photos',
-        'visibility' => 'public',
+            'driver' => 'local',
+            'root' => storage_path('app/employee_photos'),
+            'url' => env('APP_URL') . '/storage/employee_photos',
+            'visibility' => 'public',
         ],
-        ],
+
+
+    ],
 
     /*
     |--------------------------------------------------------------------------
